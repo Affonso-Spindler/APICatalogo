2e8e1c1f0b9fc6351fc449fbaf580a7bdb20113c
Affonso_Spindler <affonso_spindler@hotmail.com>
2021-02-03 18:29:12 -0300

    Adicionado Controller com métodos get e post

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,65 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        // <Injeção de dependencia>
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+
+
+
+
+        [HttpGet] //opcinal - Boa prática
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            //AsNoTracking somente em consultas, um ganho de performance
+            return _context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name="ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Produto produto)
+        {
+            //desde q use a anotação [ApiController] e seja aspNet core 2.1 ou mais, a validação do modelo é feita automaticamente
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            //inclui o produto no contexto e SaveChange "commita" essa adição
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+
+    }
+}
