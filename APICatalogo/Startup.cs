8bec648710fff7497595ec59f16a2a9145742000
Affonso_Spindler <affonso_spindler@hotmail.com>
2021-02-26 18:00:07 -0300

    Implementado autenticação JWT, e Adicionado mecanismo CORS

--- APICatalogo/Startup.cs
+++ APICatalogo/Startup.cs
@@ -4,19 +4,23 @@ using APICatalogo.Extensions;
 using APICatalogo.Filters;
 using APICatalogo.Repository;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace APICatalogo
@@ -33,6 +37,18 @@ namespace APICatalogo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //via middleware
+            services.AddCors();
+
+            //via atributo
+            //permite apenas o uso do método GET
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("PermitirApiRequest",
+                    builder =>
+                    builder.WithOrigins("http://www.apirequest.io").WithMethods("GET"));
+            });
+
             var mappingConfig = new MapperConfiguration(mc =>
             {
                 mc.AddProfile(new MappingProfile());
@@ -49,11 +65,38 @@ namespace APICatalogo
             services.AddDbContext<AppDbContext>(options =>
                                             options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<AppDbContext>()
+                    .AddDefaultTokenProviders();
+
+            /*JWT
+             * adiciona o manipulador de autenticação e define o
+             * esquema de autenticação usado : Bearer
+             * Valida o emissor, a audiencia e a chave
+             * usando a chave secreta Valida a assinatura
+             */
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+                AddJwtBearer(options =>
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidAudience = Configuration["TokenConfiguration:Audience"],
+                        ValidIssuer = Configuration["TokenConfiguration:Issuer"],
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(Configuration["Jwt:key"]))
+
+                    });
+
+
             services.AddControllers()//va ignorar a referencia cíclica na serialização da resposta Json no método action
                 .AddNewtonsoftJson(options =>
                 {
                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "APICatalogo", Version = "v1" });
@@ -77,8 +120,19 @@ namespace APICatalogo
             //Add o middleware de roteamento
             app.UseRouting();
 
+            //Add o middleware de autenticação
+            app.UseAuthentication();
+
             //Add o middleware que habilita a autorização
             app.UseAuthorization();
+
+            //Via middleware
+            //permite que API receber requisições do caminho informado
+            //app.UseCors(opt => opt.WithOrigins("https://www.apirequest.io"));
+
+            //via Atributo
+            app.UseCors();
+
 
             //Add o middleware que executa oo endpoint do Requenst Atual
             app.UseEndpoints(endpoints =>
